d6d00fae4c262bce5be9c517a7e8329a694ae25a
Em <contact.whispem@gmail.com>
2026-01-29 14:46:32 +0100

    Refactor main function to use tokens and AST

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,33 @@ mod lexer;
 mod parser;
 mod ast;
 mod interpreter;
+mod token;
 
-use std::fs;
 use lexer::Lexer;
 use parser::Parser;
 use interpreter::Interpreter;
 
 fn main() {
-    let source = fs::read_to_string("examples/hello.wsp")
-        .expect("Failed to read file");
+    let source = r#"
+# Whispem v0.2 example
+let name = "Whispem"
+print name
+"#;
 
-    let lexer = Lexer::new(&source);
-    let mut parser = Parser::new(lexer);
-    let program = parser.parse();
+    let mut lexer = Lexer::new(source);
+    let mut tokens = Vec::new();
+
+    loop {
+        let token = lexer.next_token();
+        if token == token::Token::EOF {
+            break;
+        }
+        tokens.push(token);
+    }
+
+    let mut parser = Parser::new(tokens);
+    let ast = parser.parse();
 
     let mut interpreter = Interpreter::new();
-    interpreter.run(program);
+    interpreter.execute(ast);
 }
