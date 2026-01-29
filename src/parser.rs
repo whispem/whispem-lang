b6ef32dce971845af55550a729f83662e0ed983c
Em <contact.whispem@gmail.com>
2026-01-29 14:44:38 +0100

    Refactor parser to use token vector instead of lexer

--- src/parser.rs
+++ src/parser.rs
@@ -1,110 +1,79 @@
-use crate::ast::*;
-use crate::lexer::{Lexer, Token};
+use crate::ast::{Expr, Stmt};
+use crate::token::Token;
 
 pub struct Parser {
-    lexer: Lexer,
-    current: Token,
+    tokens: Vec<Token>,
+    position: usize,
 }
 
 impl Parser {
-    pub fn new(mut lexer: Lexer) -> Self {
-        let current = lexer.next_token();
-        Self { lexer, current }
+    pub fn new(tokens: Vec<Token>) -> Self {
+        Self { tokens, position: 0 }
+    }
+
+    fn current(&self) -> &Token {
+        self.tokens.get(self.position).unwrap_or(&Token::EOF)
     }
 
     fn advance(&mut self) {
-        self.current = self.lexer.next_token();
+        self.position += 1;
     }
 
     pub fn parse(&mut self) -> Vec<Stmt> {
-        let mut stmts = Vec::new();
-        while self.current != Token::EOF {
-            stmts.push(self.statement());
-        }
-        stmts
-    }
+        let mut statements = Vec::new();
 
-    fn statement(&mut self) -> Stmt {
-        match self.current.clone() {
-            Token::Let => {
-                self.advance();
-                let name = if let Token::Ident(n) = self.current.clone() {
-                    self.advance();
-                    n
-                } else {
-                    panic!("Expected identifier");
-                };
-
-                self.advance(); // =
-                let expr = self.expr();
-                Stmt::Let { name, value: expr }
+        while !matches!(self.current(), Token::EOF) {
+            match self.current() {
+                Token::Let => statements.push(self.parse_let()),
+                Token::Print => statements.push(self.parse_print()),
+                Token::Newline => self.advance(),
+                _ => self.advance(),
             }
-            Token::Print => {
-                self.advance();
-                let expr = self.expr();
-                Stmt::Print(expr)
-            }
-            _ => panic!("Unexpected token {:?}", self.current),
         }
+
+        statements
     }
 
-    fn expr(&mut self) -> Expr {
-        let mut left = self.term();
+    fn parse_let(&mut self) -> Stmt {
+        self.advance(); // let
 
-        while matches!(self.current, Token::Plus | Token::Minus) {
-            let op = match self.current {
-                Token::Plus => Operator::Plus,
-                Token::Minus => Operator::Minus,
-                _ => unreachable!(),
-            };
-            self.advance();
-            let right = self.term();
-            left = Expr::Binary {
-                left: Box::new(left),
-                op,
-                right: Box::new(right),
-            };
-        }
-        left
+        let name = if let Token::Identifier(name) = self.current() {
+            name.clone()
+        } else {
+            panic!("Expected identifier");
+        };
+
+        self.advance(); 
+        self.advance(); 
+
+        let expr = self.parse_expr();
+        Stmt::Let(name, expr)
     }
 
-    fn term(&mut self) -> Expr {
-        let mut left = self.factor();
-
-        while matches!(self.current, Token::Star | Token::Slash) {
-            let op = match self.current {
-                Token::Star => Operator::Star,
-                Token::Slash => Operator::Slash,
-                _ => unreachable!(),
-            };
-            self.advance();
-            let right = self.factor();
-            left = Expr::Binary {
-                left: Box::new(left),
-                op,
-                right: Box::new(right),
-            };
-        }
-        left
+    fn parse_print(&mut self) -> Stmt {
+        self.advance(); // print
+        let expr = self.parse_expr();
+        Stmt::Print(expr)
     }
 
-    fn factor(&mut self) -> Expr {
-        match self.current.clone() {
+    fn parse_expr(&mut self) -> Expr {
+        match self.current() {
             Token::Number(n) => {
+                let v = *n;
                 self.advance();
-                Expr::Number(n)
+                Expr::Number(v)
             }
-            Token::Ident(name) => {
+            Token::String(s) => {
+                let v = s.clone();
                 self.advance();
-                Expr::Variable(name)
+                Expr::String(v)
             }
-            Token::LParen => {
+            Token::Identifier(name) => {
+                let v = name.clone();
                 self.advance();
-                let expr = self.expr();
-                self.advance(); // )
-                expr
+                Expr::Variable(v)
             }
-            _ => panic!("Unexpected token {:?}", self.current),
+            _ => panic!("Unexpected token"),
         }
     }
 }
