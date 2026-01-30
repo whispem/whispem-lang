144660b76fe63b3ada522769e53f107760c2785e
Em <contact.whispem@gmail.com>
2026-01-30 13:36:30 +0100

    Refactor imports and streamline file reading

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use std::env;
 use std::fs;
 
-use crate::lexer::Lexer;
-use crate::parser::Parser;
-use crate::interpreter::Interpreter;
-
 mod lexer;
 mod parser;
 mod interpreter;
 mod token;
 mod ast;
+
+use lexer::Lexer;
+use parser::Parser;
+use interpreter::Interpreter;
+use token::Token;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -19,15 +20,13 @@ fn main() {
         return;
     }
 
-    let filename = &args[1];
-    let input = fs::read_to_string(filename).expect("Failed to read file");
-
+    let input = fs::read_to_string(&args[1]).expect("Failed to read file");
     let mut lexer = Lexer::new(&input);
     let mut tokens = Vec::new();
 
     loop {
         let token = lexer.next_token();
-        if token == token::Token::EOF {
+        if token == Token::EOF {
             tokens.push(token);
             break;
         }
@@ -35,7 +34,7 @@ fn main() {
     }
 
     let mut parser = Parser::new(tokens);
-    let program = parser.parse_program(); 
+    let program = parser.parse_program();
 
     let mut interpreter = Interpreter::new();
     interpreter.execute(program);
