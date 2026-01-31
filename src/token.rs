18bd736d2cdf450d1fcad017f125579a1891e385
Em <contact.whispem@gmail.com>
2026-01-31 16:20:50 +0100

    Add Bang operator to Token enum

--- src/token.rs
+++ src/token.rs
@@ -1,37 +1,42 @@
 #[allow(dead_code)]
 #[derive(Debug, Clone, PartialEq)]
 pub enum Token {
+    // Keywords
     Let,
     Print,
     If,
     Else,
 
+    // Literals
     True,
     False,
-
     Identifier(String),
     Number(f64),
     String(String),
 
+    // Arithmetic operators
     Plus,
     Minus,
     Star,
     Slash,
 
+    // Comparison operators
     Equals,
     EqualEqual,
+    Bang,
     BangEqual,
     Less,
     LessEqual,
     Greater,
     GreaterEqual,
 
+    // Delimiters
     LParen,
     RParen,
-
     LeftBrace,
     RightBrace,
 
+    // Special
     Newline,
     EOF,
 }
